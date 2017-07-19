a1c7b63647a96a265d87f56fd11f4bbb13797a0e
Huang Chi-Han <huangchiheng@gmail.com>
2017-07-19 17:12:09 +0800

    Extract configurations to independent file. Add build api url function to conform the way to build an api url.

---
+++ env.js
@@ -0,0 +1,20 @@
+/**
+ * This is not the most optimized way to setup the config
+ */
+const devEnv = {
+  CLIENT_API_HOST: 'http://localhost:3005/api',
+  SERVER_API_HOST: 'http://localhost:3007',
+}
+
+const prodEnv = {
+  CLIENT_API_HOST: 'http://reddit-clone.itchiheng.com:3005/api',
+  SERVER_API_HOST: 'http://localhost:3007',
+}
+
+export default function env () {
+  if (process.env.NODE_ENV === 'production') {
+    return prodEnv
+  }
+
+  return devEnv
+}
--- src/apis/threads.js
+++ src/apis/threads.js
@@ -1,21 +1,43 @@
 import fetch from 'axios'
+import url from 'url'
 
-const BASE_URL = 'http://localhost:3005'
+import env from '../../env'
+
+const CLIENT_API_HOST = env().CLIENT_API_HOST
+
+/**
+ * @param {string} apiUrl
+ * @param {object} queries
+ */
+export const buildApiUrl = (apiPath, queries = {}) => {
+  const apiUrl = url.parse(`${CLIENT_API_HOST}/${apiPath}`)
+
+  // get pre-existed query
+  const query = apiUrl.query || {}
+
+  // merge queries
+  Object.assign(query, queries)
+
+  // if query object is not empty, assign query
+  if (Object.keys(query).length) apiUrl.query = query
+
+  return apiUrl.format()
+}
 
 export const fetchAllThreads = limit => (
-  fetch.get(`${BASE_URL}/api/topics?limit=${limit}`)
+  fetch.get(buildApiUrl('topics'))
   .then(response => response.data)
 )
 
 export const editThread = params => (
-  fetch.put(`${BASE_URL}/api/topic/${params.id}`, {
+  fetch.put(buildApiUrl(`topic/${params.id}`), {
     ...params,
   })
   .then(response => response.data)
 )
 
 export const createThread = topic => (
-  fetch.post(`${BASE_URL}/api/topic`, {
+  fetch.post(buildApiUrl('topic'), {
     topic,
   })
   .then(response => response.data)
