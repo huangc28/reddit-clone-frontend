e5599084e669beb68cac2a45e0b18ad2436f196d
Huang Chi-Han <huangchiheng@gmail.com>
2017-07-19 12:45:41 +0800

    Add route.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './containers/App'
-import Home from './containers/Home'
+import Threads from './containers/Threads'
 
 export default (
   <Route path="/" component={App} >
-    <IndexRoute component={Home} />
-    <Route path="/home" component={Home} />
+    <IndexRoute component={Threads} />
+    <Route path="/home" component={Threads} />
   </Route>
 )
